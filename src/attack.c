4e4fa01a747df0435280663a9f70ef34232ec48c
Stephan Günther <guenther@tum.de>
2016-05-14 15:13:18 +0200

    added some comments to attack.c

--- src/attack.c
+++ src/attack.c
@@ -8,6 +8,33 @@
 #include "global.h"
 
 
+/*
+ * This is our playground: attack() is called whenever a suspicious frame was
+ * received. The frame in question is passed to attack(). Whitelisting was
+ * already considered. It is up to you what next happens.
+ *
+ * Some examples:
+ * 1) We unconditionally mount a deauth attack by creating a deauth frame (see
+ *    deauth.c) and transmitting it.
+ * 2) Let's do that only with some probability to avoid flooding the ether with
+ *    deauths that may point to us.
+ * 3) Create a new timeout_t with some random timeout value to make sure that
+ *    this very STA will be punished for using our channel.
+ * 4) Implement a different DoS attack, e.g.
+ *    a) implement something similar to deauth by using disassociation frames
+ *       instead,
+ *    b) try to cause MIC failures in networks that use TKIP at least for
+ *       groupwise communication,
+ *    c) forge CTS to self control messages to fool the network allocation
+ *       vectors of other nodes,
+ *    d) start sending beacons on behalf of another AP and try to lure STAs to
+ *       your own faked AP,
+ *    e) ...
+ *
+ * Remember that doing this is forbidden by law.
+ */
+
+
 int
 attack(moep_frame_t frame)
 {
